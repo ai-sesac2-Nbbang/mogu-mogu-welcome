08ad3ff9f8b7144e96afc5523ef97ed567cd1719
manneungmanBaam <chanshol1998@gmail.com>
2025-10-02 16:26:59 +0900

    폰트에서 Korean 제거

--- components/fonts.tsx
+++ components/fonts.tsx
@@ -3,7 +3,7 @@ import { Do_Hyeon } from "next/font/google";
 
 export const doHyeon = Do_Hyeon({
   weight: "400",
-  subsets: ["latin", "korean"], // ✅ 한글 포함
+  subsets: ["latin"], // ✅ 한글 포함
   display: "swap",
   variable: "--font-dohyeon",
 });
